ee57d0fc925480fd64894f18752e9292a5488bc0
hungnguyen2k113-debug <hungnguyen2k113@gmail.com>
2026-03-19 05:44:28 +0700

    Add files via upload

--- clear.lua
+++ clear.lua
@@ -500,6 +500,5 @@ end)
 
 task.spawn(function()
 	task.wait(1)
-	createNotify("Cảm ơn bạn đã dùng script ❤️ 🤗 Xin chào"..LocalPlayer.Name)
-	createNotify("Tiktok: @nghbt255 | Zalo: 0394310186")
+	createNotify("Cảm ơn bạn đã dùng script ❤️ 🤗 Xin chào"..LocalPlayer.Name "Liên Hệ Tiktok: @nghbt255 | Zalo: 0394310186")
 end)
