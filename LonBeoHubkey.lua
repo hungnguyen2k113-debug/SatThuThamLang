a4a8c0a672336123eb021fb2b49a5a980590c98d
hungnguyen2k113-debug <hungnguyen2k113@gmail.com>
2026-03-19 11:50:46 +0700

    Add files via upload

---
+++ LonBeoHubkey.lua
@@ -0,0 +1,15 @@
+repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+getgenv().Key = "c1f68f415eb48ae522bb88d98"
+
+local inputKey = getgenv().KeyInput or ""
+
+if inputKey ~= getgenv().Key then
+	LocalPlayer:Kick("The key is incorrect or has already been used.")
+	return
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hungnguyen2k113-debug/SatThuThamLang/main/lonbeoHub.lua"))()
